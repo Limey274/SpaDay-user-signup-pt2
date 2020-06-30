387f55ab4f89da7151b37fc6c05b7ed354904c77
Daniel <d.lalime7@gmail.com>
2020-06-30 07:26:47 -0700

    Add project files.

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using SpaDay.ViewModel;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+            return View(addUserViewModel);
+        }
+
+        public IActionResult Add()
+        { 
+           AddUserViewModel addUserViewModel = new AddUserViewModel();
+          
+            return View(addUserViewModel);
+
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                if (addUserViewModel.Password == addUserViewModel.Verify)
+                {
+                    User newUser = new User
+                    {
+                        Username = addUserViewModel.Username,
+                        Email = addUserViewModel.Email,
+                        Password = addUserViewModel.Password,
+                        Verify = addUserViewModel.Verify
+
+                    };
+
+                    return View("Index", newUser);
+                }
+                else
+                {
+                    return View("add");
+                }
+               
+
+            }
+            else
+            {
+                return View("add", addUserViewModel);
+            }
+
+
+            
+        }
+
+    }
+}
---
+++ SpaDay/ViewModel/AddUserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.ViewModel
+{
+    public class AddUserViewModel
+    {
+
+
+        [Required(ErrorMessage = "Please Enter a Username")]
+        [StringLength(15, MinimumLength = 5)]
+        public string Username { get; set; }
+
+
+        //[Required(ErrorMessage ="Please Enter your Email Address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter a Password")]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Please Verify your Password")]
+        [Compare("Password", ErrorMessage = "The two passwords must match")]
+        public string Verify { get; set; }
+
+      }
+}
